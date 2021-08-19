5e13c0a22316f1f4eda9919fec246dcc59dda524
JuanVasquez <jd.vasquezhz@gmail.com>
2021-08-18 21:27:56 -0500

    Se modifico el menu de opciones

--- App/view.py
+++ App/view.py
@@ -78,7 +78,8 @@ while True:
 
     elif int(inputs[0]) == 3:
         print("Cargando información de tags de libros....")
-        
+        input("Presione 'Enter' para continuar.\n")
+
     else:
         sys.exit(0)
 sys.exit(0)
